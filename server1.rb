b187444b0517368adcae473437cf6e657833c6db
Rikuto Shimizu <k26gjk07@st.kyusan-u.ac.jp>
2026-06-29 03:18:28 +0000

    サーバー処理をメソッド化

--- server1.rb
+++ server1.rb
@@ -2,17 +2,19 @@
 
 require 'socket'
 
-gs = TCPServer.open 'http'
-s = gs.accept
-
-loop do
-  pp "start accept"
-  s = gs.accept
+def server s
   while line = s.gets
     pp line
     s.puts line
     break if line == "\r\n"
   end
-s.close
-sleep 10
+  s.close
+end
+
+gs = TCPServer.open 'http'
+loop do
+  pp "start accept"
+  s = gs.accept
+  server s
+  sleep 10
 end
