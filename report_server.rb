e1142ab68ac2aacbf2d70df54406cb5270a6065d
Rikuto Shimizu <k26gjk07@st.kyusan-u.ac.jp>
2026-07-29 17:48:54 +0000

    サンプルデータから、本データで試行し成功した。

--- report_server.rb
+++ report_server.rb
@@ -63,7 +63,7 @@ end
 #------------------------------
 
 def server s, entry_count, exit_count, max_count, min_count
-  (10..17).each do |hour|
+  (6..23).each do |hour|
     s.puts "#{hour},#{entry_count[hour]},#{exit_count[hour]},#{max_count[hour]},#{min_count[hour]}"
   end
 
